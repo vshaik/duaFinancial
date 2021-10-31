ec05c41ad77ad1b8a914097f86202bf3a634761e
Vali Shaik <reachvali@gmail.com>
2021-10-31 10:09:55 -0500

    2021 reports

---
+++ dua-financial/src/main/java/com/dua/finance/CommunityOutreach.java
@@ -0,0 +1,13 @@
+package com.dua.finance;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CommunityOutreach {
+
+	public static final Logger logger = LogManager.getLogger(CommunityOutreach.class);
+	public static void main(String[] args) {
+
+	}
+
+}
